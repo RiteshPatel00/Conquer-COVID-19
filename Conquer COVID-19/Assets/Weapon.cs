07e941b10ae9f73ae6343dc9bd4633c1a7fdf690
Ritesh Patel <patel2000ritesh@gmail.com>
2020-07-29 13:56:49 -0400

    Update

---
+++ Conquer COVID-19/Assets/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        transform.rotation = rotation;
+
+
+    }
+}
