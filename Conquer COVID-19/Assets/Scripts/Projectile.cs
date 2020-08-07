614e49f6bdcb5110245fdcec494eb4c944d86804
Ritesh Patel <patel2000ritesh@gmail.com>
2020-08-06 20:08:48 -0400

    Created enemy character

--- Conquer COVID-19/Assets/Scripts/Projectile.cs
+++ Conquer COVID-19/Assets/Scripts/Projectile.cs
@@ -8,6 +8,7 @@ public class Projectile : MonoBehaviour
     public float speed;
     public float lifeTime;
     public GameObject explosion;
+    public int damage;
 
     // Start is called before the first frame update
     void Start()
@@ -27,10 +28,20 @@ public class Projectile : MonoBehaviour
 
 
     void DestroyProjectile(){
-        
+        Destroy(gameObject);
         Instantiate(explosion, transform.position, Quaternion.identity);
 
-        Destroy(gameObject);
         
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision){
+
+        if(collision.tag == "Enemy"){
+            collision.GetComponent<Enemy>().TakeDamage(damage);
+            DestroyProjectile();
+        }
+
+
     }
 }
